ae109e1874d1a2e5a17288e9283e6a1874b10aa0
Jia <l19960108@gmail.com>
2019-12-11 16:54:05 -0500

    user home component

--- movie-client/src/app/app.module.ts
+++ movie-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserHomeComponent } from './components/user-home/user-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule,
