e8430f954ec8d25c50c8c9d1350289410b28bd0e
bobbyb88 <rbucci@oldwestbury.edu>
2019-12-18 19:49:40 -0500

    New Features

--- movie-client/src/app/services/review-service/review.service.ts
+++ movie-client/src/app/services/review-service/review.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Review } from '../../models/review';
 import { Observable } from 'rxjs';
 
@@ -8,6 +8,14 @@ import { Observable } from 'rxjs';
 })
 export class ReviewService {
 
+  review: Review = {
+    reviewId: 0,
+    movieId:0,
+    userId:0,
+    postTitle:" ",
+    postBody: " ",
+
+  };
   url: string = 'http://localhost:8080/PBJCinema/reviews';
 
   constructor(private http: HttpClient) { }
@@ -22,5 +30,24 @@ export class ReviewService {
 
   getReviewsByUserId(idParam: number) {
     // return this.http.get<Review>(this.url + '/' + idParam).toPromise();
+  }
+
+  editReview(userid, postTitle,postBody) {
+   this.review.userId=userid;
+    this.review.postTitle=postTitle;
+    this.review.postBody=postBody;
+   
+
+
+  let headers = new HttpHeaders();
+headers.append('Content-Type', 'application/json');
+
+this.http.post(this.url,this.review).subscribe(
+  (response) => console.log(response),
+  (error) => console.log(error)
+);
+return true;
+
+    
   }
 }
